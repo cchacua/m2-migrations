d6fd3e85305b59938e9799c37681a6520274c692
Christian Chacua <cchacua@gmail.com>
2017-10-24 23:21:36 +0200

    24102017 1921

--- scripts/sql/queries.R
+++ scripts/sql/queries.R
@@ -21,6 +21,10 @@ q2<- dbSendQuery(patstat, "SELECT patstat2016b.TLS201_APPLN.APPLN_ID, patstat201
 apn<-dbFetch(q2)
 dbClearResult(q2)
 
+q2<- dbSendQuery(patstat, "SELECT CONCAT(PUBLN_AUTH,PUBLN_NR) AS pub_number FROM patstat2016b.TLS211_PAT_PUBLN   LIMIT 1000, 1100;")
+apn211<-dbFetch(q2)
+dbClearResult(q2)
+
 q3<- dbSendQuery(patstat, "SELECT patstat2016b.TLS201_APPLN.APPLN_ID, 
                                   patstat2016b.TLS211_PAT_PUBLN.PUBLN_NR,
                                   patstat2016b.TLS201_APPLN.DOCDB_FAMILY_ID, 
@@ -32,21 +36,21 @@ q3<- dbSendQuery(patstat, "SELECT patstat2016b.TLS201_APPLN.APPLN_ID,
                             INNER JOIN patstat2016b.TLS211_PAT_PUBLN
                             ON patstat2016b.TLS201_APPLN.APPLN_ID=patstat2016b.TLS211_PAT_PUBLN.APPLN_ID
                             LIMIT 0, 100;")
-apn<-dbFetch(q3)
+apn<-dbFetch(q3, n=100)
 dbClearResult(q3)
 
 # Riccaboni
 dbListTables(riccaboni)
 dbListFields(riccaboni, "t01")
 
-q2<- dbSendQuery(riccaboni, "SELECT RIGHT(riccaboni.t01.pat, LENGTH(riccaboni.t01.pat)-2) AS pat, riccaboni.t01.invs, riccaboni.t01.localInvs, riccaboni.t01.apps, riccaboni.t01.yr, riccaboni.t01.classes, riccaboni.t01.wasComplete FROM riccaboni.t01  LIMIT 1000, 1100;")
-apn<-dbFetch(q2)
+q2<- dbSendQuery(riccaboni, "SELECT RIGHT(riccaboni.t01.pat, LENGTH(riccaboni.t01.pat)-2) AS pat, riccaboni.t01.invs, riccaboni.t01.localInvs, riccaboni.t01.apps, riccaboni.t01.yr, riccaboni.t01.classes, riccaboni.t01.wasComplete FROM riccaboni.t01  LIMIT 0, 100;")
+apn2<-dbFetch(q2)
 dbClearResult(q2)
 
 
 q3<- dbSendQuery(riccaboni, "SELECT riccaboni.t01.pat FROM riccaboni.t01;")
-ric.pn<-dbFetch(q3,  n = -1)
+ric.pn<-dbFetch(q3)
 dbClearResult(q3)
 save(ric.pn, file="../output/riccabonit01pat.RData")
-
+ric.pn<-as.data.frame(ric.pn)
 
--- sqlscripts/queries/tables1.sql
+++ sqlscripts/queries/tables1.sql
@@ -3,3 +3,126 @@ SELECT TLS201_APPLN.APPLN_ID FROM TLS201_APPLN  LIMIT 0, 10 INTO OUTFILE '/var/l
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
+
+
+-- Query three tables
+USE riccaboni patstat2016b
+SELECT patstat2016b.TLS201_APPLN.APPLN_ID, 
+                                  patstat2016b.TLS211_PAT_PUBLN.PUBLN_NR,
+                                  RIGHT(riccaboni.t01.pat, LENGTH(riccaboni.t01.pat)-2) AS pat,
+                                  patstat2016b.TLS201_APPLN.DOCDB_FAMILY_ID, 
+                                  patstat2016b.TLS201_APPLN.DOCDB_FAMILY_SIZE, 
+                                  patstat2016b.TLS201_APPLN.INPADOC_FAMILY_ID, 
+                                  patstat2016b.TLS201_APPLN.NB_APPLICANTS, 
+                                  patstat2016b.TLS201_APPLN.NB_INVENTORS 
+                                  riccaboni.t01.invs, 
+                                  riccaboni.t01.localInvs, 
+                                  riccaboni.t01.apps, 
+                                  riccaboni.t01.yr, 
+                                  riccaboni.t01.classes, 
+                                  riccaboni.t01.wasComplete 
+                                  
+                            FROM riccaboni.t01  
+                            INNER JOIN patstat2016b.TLS211_PAT_PUBLN ON pat=patstat2016b.TLS211_PAT_PUBLN.PUBLN_NR
+                            INNER JOIN patstat2016b.TLS201_APPLN
+                                ON patstat2016b.TLS201_APPLN.APPLN_ID=patstat2016b.TLS211_PAT_PUBLN.APPLN_ID
+                            LIMIT 0, 100
+INTO OUTFILE '/var/lib/mysql-files/three-joins.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
+
+
+--
+USE riccaboni;
+    SELECT    RIGHT(riccaboni.t01.pat, LENGTH(riccaboni.t01.pat)-2) AS pat,
+              riccaboni.t01.invs, 
+              riccaboni.t01.localInvs, 
+              riccaboni.t01.apps, 
+              riccaboni.t01.yr, 
+              riccaboni.t01.classes, 
+              riccaboni.t01.wasComplete FROM riccaboni.t01
+INTO OUTFILE '/var/lib/mysql-files/only-riccaboni.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
+
+--
+
+USE riccaboni patstat2016b
+SELECT RIGHT( riccaboni.t01.pat, LENGTH(riccaboni.t01.pat)-2) AS pat,
+              patstat2016b.TLS211_PAT_PUBLN.PUBLN_NR,
+              riccaboni.t01.invs, 
+              riccaboni.t01.localInvs, 
+              riccaboni.t01.apps, 
+              riccaboni.t01.yr, 
+              riccaboni.t01.classes, 
+              riccaboni.t01.wasComplete 
+              FROM patstat2016b.TLS211_PAT_PUBLN 
+              INNER JOIN riccaboni.t01 ON patstat2016b.TLS211_PAT_PUBLN.PUBLN_NR=pat
+INTO OUTFILE '/var/lib/mysql-files/riccaboni-211-.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
+-- ONLY 26865
+
+SELECT riccaboni.t01.pat AS pat
+       FROM patstat2016b.TLS211_PAT_PUBLN 
+       INNER JOIN riccaboni.t01 ON CONCAT(patstat2016b.TLS211_PAT_PUBLN.PUBLN_AUTH, patstat2016b.TLS211_PAT_PUBLN.PUBLN_NR)=riccaboni.t01.pat
+INTO OUTFILE '/var/lib/mysql-files/riccaboni-211---.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
+-- TOTAL- 7.146.247 vs 9.290.268 patents in Riccaboni
+
+-- CONCAT(patstat2016b.TLS211_PAT_PUBLN.PUBLN_AUTH, patstat2016b.TLS211_PAT_PUBLN.PUBLN_NR) AS pnumber,
+--              riccaboni.t01.invs, 
+--              riccaboni.t01.localInvs, 
+--              riccaboni.t01.apps, 
+--              riccaboni.t01.yr, 
+--              riccaboni.t01.classes, 
+--              riccaboni.t01.wasComplete 
+
+
+SELECT patstat2016b.TLS211_PAT_PUBLN.PUBLN_NR
+              FROM patstat2016b.TLS211_PAT_PUBLN 
+INTO OUTFILE '/var/lib/mysql-files/pat_pub_number211.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
+
+
+-- Query three tables
+USE riccaboni patstat2016b
+SELECT        riccaboni.t01.pat,
+              patstat2016b.TLS211_PAT_PUBLN.PUBLN_NR,
+              patstat2016b.TLS211_PAT_PUBLN.PUBLN_AUTH, 
+              patstat2016b.TLS201_APPLN.APPLN_ID, 
+              patstat2016b.TLS201_APPLN.DOCDB_FAMILY_ID, 
+              patstat2016b.TLS201_APPLN.DOCDB_FAMILY_SIZE, 
+              patstat2016b.TLS201_APPLN.INPADOC_FAMILY_ID, 
+              patstat2016b.TLS201_APPLN.NB_APPLICANTS, 
+              patstat2016b.TLS201_APPLN.NB_INVENTORS, 
+              riccaboni.t01.invs, 
+              riccaboni.t01.localInvs, 
+              riccaboni.t01.apps, 
+              riccaboni.t01.yr, 
+              riccaboni.t01.classes, 
+              riccaboni.t01.wasComplete 
+              FROM patstat2016b.TLS211_PAT_PUBLN 
+              INNER JOIN riccaboni.t01 ON CONCAT(patstat2016b.TLS211_PAT_PUBLN.PUBLN_AUTH, patstat2016b.TLS211_PAT_PUBLN.PUBLN_NR)=riccaboni.t01.pat
+              INNER JOIN patstat2016b.TLS201_APPLN ON patstat2016b.TLS211_PAT_PUBLN.APPLN_ID=patstat2016b.TLS201_APPLN.APPLN_ID
+INTO OUTFILE '/var/lib/mysql-files/riccaboni-211-merge.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
+
+
+SELECT DISTINCT patstat2016b.TLS201_APPLN.DOCDB_FAMILY_ID
+              FROM patstat2016b.TLS211_PAT_PUBLN 
+              INNER JOIN riccaboni.t01 ON CONCAT(patstat2016b.TLS211_PAT_PUBLN.PUBLN_AUTH, patstat2016b.TLS211_PAT_PUBLN.PUBLN_NR)=riccaboni.t01.pat
+              INNER JOIN patstat2016b.TLS201_APPLN ON patstat2016b.TLS211_PAT_PUBLN.APPLN_ID=patstat2016b.TLS201_APPLN.APPLN_ID
+INTO OUTFILE '/var/lib/mysql-files/riccaboni-211-docfamily.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
