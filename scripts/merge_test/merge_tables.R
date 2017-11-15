a04de2d5e940b9a7121ee7cedab5d8229433aad9
Christian Chacua <cchacua@gmail.com>
2017-11-15 19:29:09 +0100

    15112017 1929

--- scripts/merge_test/merge_tables.R
+++ scripts/merge_test/merge_tables.R
@@ -7,3 +7,18 @@ query.latex("SELECT COUNT(*) As 'Number of patents', CHAR_LENGTH(a.pat) AS 'Numb
             0, 
             c("Table 1", "T1"), 
             "t1-sd")
+
+query.latex("SELECT COUNT(*) As 'Number of patents', LEFT(a.pat,2) AS 'Patent authority'
+      FROM riccaboni.t01 a
+      GROUP BY LEFT(a.pat,2);", 
+            0, 
+            c("Number of patents by publication authority in Morrison2017 database", "Number of patents in Morrison2017 database"), 
+            "table-ric-number-pat-by-aut")
+
+query.latex("SELECT COUNT(*), LEFT(a.pat,2) AS Authority
+      FROM riccaboni.t01 a
+      WHERE a.yr>1977 AND a.yr<2010
+      GROUP BY LEFT(a.pat,2);", 
+            0, 
+            c("Number of patents by publication authority in Morrison2017 database from 1978", "Number of patents in Morrison2017 database"), 
+            "table-ric-number-pat-by-aut-1978")
